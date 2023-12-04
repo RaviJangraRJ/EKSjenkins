207c8c47e2c1d8b880f7a387e92dd0f599782f6f
RaviJangraRJ <100367393+RaviJangraRJ@users.noreply.github.com>
2023-12-04 20:49:18 +0530

    Update backend.tf

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "terraform-eks-ravi"
     key    = "eks/terraform.tfstate"
-    dynamoDB = "terraform-lock"
+    dynamodb_table  = "terraform-lock"
     region = "ap-south-1"
   }
 }
